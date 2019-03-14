1fabfb9f349b3500f3dc27b2144d2a5a1a56c555
Stefan Tapper <tapper.stefan@gmail.com>
2019-03-14 14:30:56 +0100

    update welcome page

--- modules/gui/widgets/welcome_page.py
+++ modules/gui/widgets/welcome_page.py
@@ -21,33 +21,61 @@ class KnechtWelcome(QWidget):
 
         :param modules.gui.main_ui.KnechtWindow ui: Knecht main window
         """
-        super(KnechtWelcome, self).__init__(ui)
+        super(KnechtWelcome, self).__init__()
         SetupWidget.from_ui_file(self, Resource.ui_paths['knecht_welcome'])
         self.ui = ui
         self.setWindowTitle(_('Willkommen'))
 
         self.title_label.setText(_('RenderKnecht v{}').format(KnechtSettings.app['version']))
+        self.create_label.setText(_('Erstellen'))
+        self.create_label.setStyleSheet('font-weight: 800;')
+        self.recent_label.setText(_('Kürzlich verwendet'))
+        self.recent_label.setStyleSheet('font-weight: 800;')
+
+        self.new_btn.setText(_('Neues Dokument'))
+        self.new_btn.released.connect(self.ui.main_menu.file_menu.new_document)
+
+        self.open_btn.setText(_('Dokument öffnen'))
+        self.open_btn.released.connect(self._open)
+
+        self.import_btn: QPushButton
+        self.import_btn.setText(_('Import'))
+        self.import_btn.setMenu(self.ui.main_menu.file_menu.import_menu)
 
         self.recent_layout: QVBoxLayout = self.recent_layout
         self.recent_btns = list()
 
+        # --- Update recent files ---
+        self.ui.main_menu.file_menu.update_recent_files_menu()
         self.update()
+        self.ui.main_menu.file_menu.recent_menu.aboutToShow.connect(self.update)
+
+    def _open(self):
+        self.ui.main_menu.file_menu.open_xml()
+
+    def _import(self):
+        self.ui.main_menu.file_menu.import_menu.popup(self.import_btn.pos())
 
     def update(self):
         self.setUpdatesEnabled(False)
-        self.ui.main_menu.file_menu.update_recent_files_menu()
 
         while self.recent_btns:
             btn = self.recent_btns.pop()
             self.recent_layout.removeWidget(btn)
             btn.deleteLater()
 
+        spacer = self.recent_layout.takeAt(0)
+
         for action in self.ui.main_menu.file_menu.recent_menu.actions():
             btn = QPushButton(action.text(), self)
             btn.setIcon(action.icon())
-            btn.setStyleSheet('background: transparent;')
             btn.released.connect(action.trigger)
             self.recent_btns.append(btn)
             self.recent_layout.addWidget(btn, alignment=Qt.AlignLeft)
+
+            if len(self.recent_btns) > 5:
+                break
+
+        self.recent_layout.addSpacerItem(spacer)
 
         self.setUpdatesEnabled(True)
