4fba7fd5b46e2cfd90efd625add552910477a8a6
Stefan Tapper <tapper.stefan@gmail.com>
2019-04-03 11:03:17 +0200

    update fakom pattern

--- modules/knecht_fakom.py
+++ modules/knecht_fakom.py
@@ -27,7 +27,7 @@ class FakomPattern:
     pattern.append((f'^{fa}_on_{lum}_on_{sib}_on$', (0, 2, -6, -3)))
 
     # LUM_on_FA_on_SIB_on
-    pattern.append((f'^{lum}_on_{fa}_on_{sib}_on$', (-6, -3, -12, -10)))
+    pattern.append((f'^{lum}_on_{fa}_on_{sib}_on$', (-12, -10, -6, -3)))
 
     # FA_SIB
     pattern.append((f'^{fa}_{sib}$', (0, 2, 3, 6)))
@@ -36,7 +36,7 @@ class FakomPattern:
     pattern.append((f'^{fa}_{sib}_{sib}$', (0, 2, 3, 6)))
 
     # LUM_FA_SIB_on
-    pattern.append((f'^{lum}_{fa}_{sib}_on$', (-6, -3, -9, -7)))
+    pattern.append((f'^{lum}_{fa}_{sib}_on$', (-9, -7, -6, -3)))
 
     @classmethod
     def search(cls, action_list_name) -> Union[None, Tuple[str, str]]:
