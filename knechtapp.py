da8dca4fc129ca3c025742f7035f96baea47d498
Stefan Tapper <tapper.stefan@gmail.com>
2019-12-17 15:16:02 +0100

    update KnechtViewer to 1.4

--- knechtapp.py
+++ knechtapp.py
@@ -19,7 +19,7 @@ if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
     print('Using high dpi Pixmaps')
 
-VERSION = '1.361'
+VERSION = '1.362'
 
 InfoMessage.ver = VERSION
 InfoMessage.lic = 'GPL v3'
