62a124f33bcbd1bbddbb270489d4410d28246e54
Stefan Tapper <tapper.stefan@gmail.com>
2020-09-01 15:28:34 +0200

    update plmxml module

--- knechtapp.py
+++ knechtapp.py
@@ -19,7 +19,7 @@ if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
     print('Using high dpi Pixmaps')
 
-VERSION = '1.431'
+VERSION = '1.432'
 
 InfoMessage.ver = VERSION
 InfoMessage.lic = 'GPL v3'
